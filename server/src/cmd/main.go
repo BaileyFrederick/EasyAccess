e19b9552f6f93ae5d680bb2b4c482da1d10caacd
Bailey Frederick <FrederickBailey18@gmail.com>
2019-09-17 14:40:15 -0400

    set up firestore

--- server/src/cmd/main.go
+++ server/src/cmd/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	firebase "firebase.google.com/go"
 	"github.com/joho/godotenv"
-	"google.golang.org/api/option"
 )
 
 func init() {
@@ -19,13 +17,32 @@ func init() {
 
 func main() {
 
-	// key, EXISTS := os.LookupEnv("KEY")
-	// if EXISTS {
-	// 	print(key)
-	// }
-	opt := option.WithCredentialsFile("./serviceAccountKey.json")
-	_, err := firebase.NewApp(context.Background(), nil, opt)
+	ctx := context.Background()
+
+	//creates new firebase app connection based on GOOGLE_APPLICATION_CREDENTIALS in .env file
+	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
-		print(fmt.Errorf("error initializing app: %v", err))
+		log.Fatalf("error initializing app: %v\n", err)
 	}
+
+	//creates connection to Firestore database
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//closes client when code finishes
+	defer client.Close()
+
+	p := Person{
+		Name: "Alice",
+	}
+	//Changes the name of the specific user based on UID to ALICE
+	_, err = client.Collection("users").Doc("755O4T422rS1CgngVpI8").Set(ctx, p)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Person struct {
+	Name string
 }
